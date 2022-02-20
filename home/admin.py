bc455cbe6d72445ab9ce5eb1a110a4ab4583b3d1
zaid kamil <xaidmetamorphos@gmail.com>
2022-02-20 17:28:10 +0530

    admin dashboard

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,16 @@
-from atexit import register
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Product)
+# admin.site.register(Product) ❌
+
+# 🎯 adminview  shortcut
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('procode','title','brand','added_on')
+    ordering = ('brand',)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title',
+    'is_published','created_on')
--- home/models.py
+++ home/models.py
@@ -7,3 +7,13 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     brand = models.CharField(max_length=20)
     added_on = models.DateTimeField() 
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    image = models.ImageField(upload_to ="articles")
+    content = models.TextField()
+    is_published = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now=True)
+
+# create a model contact with name, email, phone, message
